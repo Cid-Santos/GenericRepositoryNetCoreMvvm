420989a9292942653ef47e5ee7a2a10405f38dfa
Cid Santos <orium-5@hotmail.com>
2020-03-22 16:12:51 -0300

    Comentarios da classe Repository

--- DataAcess/Repository/Repository.cs
+++ DataAcess/Repository/Repository.cs
@@ -4,13 +4,14 @@ using SQLite;
 
 namespace DataAcess.Repository
 {
-    public class ProdutoRepository : RepositoryBase<Product>, IProductRepository
-    {
-        public ProdutoRepository(SQLiteAsyncConnection connection): base(connection) { }
-    }
-
     public class PersonRepository : RepositoryBase<Person>, IPersonRepository
     {
         public PersonRepository(SQLiteAsyncConnection connection) : base(connection) { }
     }
+
+    public class ProdutoRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProdutoRepository(SQLiteAsyncConnection connection) : base(connection) { }
+    }
+
 }
--- DataAcess/Repository/RepositoryBase.cs
+++ DataAcess/Repository/RepositoryBase.cs
@@ -10,27 +10,97 @@ namespace DataAcess.Repository
     public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class, new()
     {
         protected readonly SQLiteAsyncConnection Connection;
+        // Acompanha se Dispose foi chamado.
+        private bool disposed = false;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connection"></param>
         public RepositoryBase(SQLiteAsyncConnection connection)
         {
             this.Connection = connection;
         }
+
+        /// <summary>
+        /// O padrão para o descarte de um objeto, conhecido como padrão Dispose, impõe ordem no tempo de vida de um objeto.
+        /// (GC) Força uma coleta de lixo imediata, este objeto será limpo pelo método Dispose. 
+        ///  Portanto, você deve chamar GC.SupressFinalize para retira esse objeto da fila de finalização.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
+
+        /// <summary>
+        /// Metodo para fechar a conexão do banco SQLite, utiliza um parametro para 
+        /// fechar a conexao (disposing = True)
+        /// </summary>
+        /// <param name="disposing">Parametro booleano</param>
         protected virtual void Dispose(bool disposing)
         {
-            if (disposing)
+            //Verifique se Dispose já foi chamado.
+            if (!this.disposed)
             {
-                Connection?.CloseAsync();
+                // Se o disposing for igual a true, descarta todos os
+                // e recursos não gerenciados.
+                if (disposing)
+                {
+                    // Descarta recursos gerenciados.
+                    Connection?.CloseAsync();
+                }
+                //Se o Dispose foi feito.
+                disposed = true;
             }
         }
-        public AsyncTableQuery<TEntity> AsQueryable() => Connection.Table<TEntity>();
-        public async Task<TEntity> Get(int id) => await Connection.FindAsync<TEntity>(id);
-        public async Task<TEntity> Get(Expression<Func<TEntity, bool>> predicate) => await Connection.FindAsync<TEntity>(predicate);
-        public async Task<List<TEntity>> Get() => await Connection.Table<TEntity>().ToListAsync();
+
+        /// <summary>
+        /// Inicializa uma nova instância da classe AsyncTableQuery <T>.
+        /// </summary>
+        /// <returns></returns>
+        public AsyncTableQuery<TEntity> AsQueryable()
+        {
+            return Connection.Table<TEntity>();
+        }
+
+        /// <summary>
+        ///  Metodo para buscar o objeto generico por Id 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<TEntity> Get(int id)
+        {
+            return await Connection.FindAsync<TEntity>(id);
+        }
+
+        /// <summary>
+        /// Metodo para buscar o objeto generico atraves de uma expressão lambida com predicato
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public async Task<TEntity> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await Connection.FindAsync<TEntity>(predicate);
+        }
+
+        /// <summary>
+        /// Metodo para buscar todos os items  de um objeto generico
+        /// </summary>
+        /// <returns>Retorna uma lista do tipo generico</returns>
+        public async Task<List<TEntity>> Get()
+        {
+            return await Connection.Table<TEntity>().ToListAsync();
+        }
+
+        /// <summary>
+        /// Metodo para buscar o objeto generico atraves de uma expressão lambida com predicato
+        /// e com ordenação dos dados 
+        /// </summary>
+        /// <typeparam name="TValue">tipo de ordenação</typeparam>
+        /// <param name="predicate">valor da consulta</param>
+        /// <param name="orderBy">Valor a ser ordenado</param>
+        /// <returns>Retorna uma lista do tipo generico ordenada</returns>
         public async Task<List<TEntity>> Get<TValue>(Expression<Func<TEntity, bool>> predicate = null, Expression<Func<TEntity, TValue>> orderBy = null)
         {
             var query = Connection.Table<TEntity>();
@@ -43,8 +113,35 @@ namespace DataAcess.Repository
 
             return await query.ToListAsync();
         }
-        public async Task<int> Insert(TEntity entity) =>  await Connection.InsertAsync(entity);
-        public async Task<int> Update(TEntity entity) => await Connection.UpdateAsync(entity);
-        public async Task<int> Delete(TEntity entity) => await Connection.DeleteAsync(entity);
+
+        /// <summary>
+        /// Insere dados de um objeto generio em um objeto generico SQLite
+        /// </summary>
+        /// <param name="entity">Parâmetro de tipo de dados a ser utilizados</param>
+        /// <returns>Retorna o estado do objeto</returns>
+        public async Task<int> Insert(TEntity entity)
+        {
+            return await Connection.InsertAsync(entity);
+        }
+
+        /// <summary>
+        /// Altera os dados de um objeto generio em um objeto generico SQLite
+        /// </summary>
+        /// <param name="entity">Parâmetro de tipo de dados a ser utilizados</param>
+        /// <returns>Retorna o estado do objeto</returns>
+        public async Task<int> Update(TEntity entity)
+        {
+            return await Connection.UpdateAsync(entity);
+        }
+
+        /// <summary>
+        /// Salva os dados de um objeto generio em um objeto generico SQLite
+        /// </summary>
+        /// <param name="entity">Parâmetro de tipo de dados a ser utilizados</param>
+        /// <returns>Retorna o estado do objeto</returns>
+        public async Task<int> Delete(TEntity entity)
+        {
+            return await Connection.DeleteAsync(entity);
+        }
     }
 }
